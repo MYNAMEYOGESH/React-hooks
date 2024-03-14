23bf04c71d1bf5fd32c6a74d5ed82059e22aa2c1
MYNAMEYOGESH <2001yogesh2001yogesh@gmail.com>
2024-03-14 13:57:53 +0530

    14/3/24

--- src/screens/Ex8.jsx
+++ src/screens/Ex8.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import React,{useMemo, useState} from 'react'
+import Marks from '../pages/Marks'
+import Info from '../pages/Info'
 
 function Ex8() {
+  const [age,setAge] = useState(10)
+
+  const incAge = (val) => {
+    setAge(age + val)
+  }
+  const rajsAge = useMemo(()=> ({
+    result: `Raj's age is ${age} years`
+  }),[age])
+
   return (
     <div className="container">
     <div className="row">
@@ -8,6 +19,8 @@ function Ex8() {
             <h3 className="display-3 text-warning">useMemo hook</h3>
         </div>
     </div>
+    <Marks age={age} add={incAge}/>
+    <Info txt= {rajsAge} />
    </div>
   )
 }
